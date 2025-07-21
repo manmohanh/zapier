2b7cddf42a024a8c46eca5b4632df9ddba90e0b7
Manmohan Hansda <hmanmohan12@gmail.com>
2025-07-21 17:35:16 +0530

    updates

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,3 +1,30 @@
-import express from "express"
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
-const app = express()
+const app = express();
+app.use(express.json());
+const client = new PrismaClient();
+
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const { userId, zapId } = req.params;
+  const body = req.body;
+
+  await client.$transaction(async (tx) => {
+    const run = await tx.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: body,
+      },
+    });
+
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+  });
+
+  res.json({ message: "Webhook Received" });
+});
+
+app.listen(5000);
